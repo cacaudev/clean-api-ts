49eb11c1c03c6c35284c5c5fce4c85fd2d405c4f
cacaudev <cacauabreu2013@gmail.com>
2020-07-15 19:05:12 -0300

    [fix] (App): Add error handler for app

--- src/server.ts
+++ src/server.ts
@@ -2,20 +2,35 @@
  * @Author: cacaudev
  * @Date: 2020-07-12 17:39:28
  * @Last Modified by: cacaudev
- * @Last Modified time: 2020-07-15 18:12:44
+ * @Last Modified time: 2020-07-15 19:03:55
  */
 import App from './app';
 import { logger } from './loaders';
 
+const PORT = process.env.port || 3000;
 const app = new App();
 
-app.on('error', (err) => {
-  logger.error(`Error on app: ${err}`);
-  // TODO: Add response instance for error
-});
+const onAppError = (error: NodeJS.ErrnoException): void => {
+  if (error.syscall !== 'listen') throw error;
+  switch (error.code) {
+    case 'EACCES':
+      logger.error(`Port ${PORT} requires elevated privileges.`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      logger.error(`Port ${PORT} is already in use.`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+const onAppListening = (): void => {
+  const serverAddress = server.address();
+  logger.info(`[SERVER] Running at http://localhost:${serverAddress.port}`);
+};
 
-const server = app.listen(3000, () => {
-  logger.info('[SERVER] Running at http://localhost:3000');
-});
+const server = app.listen(PORT, onAppListening);
+app.on('error', onAppError);
 
 export default server;
