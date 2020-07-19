68bb414744fd8f07a653d6ca5a0b2ded916d3295
cacaudev <cacauabreu2013@gmail.com>
2020-07-19 20:40:24 -0300

    [fix] (Package): Fix jest config + Fix build command + Fix babel config

---
+++ .babelrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ],
+    "@babel/preset-typescript"
+  ],
+  plugins: [
+    ['module-resolver', {
+      alias: {
+        '@config': './src/config',
+        '@models': './src/models',
+        '@controllers': './src/controllers',
+        '@routers': './src/api/routers',
+        '@common': './src/common',
+        '@services': './src/services'
+      }
+    }]
+  ],
+  ignore: [
+    '**/*.spec.ts'
+  ]
+}
--- src/__tests__/app.spec.ts
+++ src/__tests__/app.spec.ts
@@ -2,13 +2,7 @@ import supertest from 'supertest';
 import http from 'http';
 import App from '../app';
 
-/**
- * OBS: Using http.createServer() as workaround
- * for supertest need for http.Server to work on.
- * Calling app.listen() to get a server will
- * start a listening server, this is bad
- * practice and unnecessary.
- */
+
 
 describe('Integration App Test', () => {
   test('[GET] /', (done) => {
